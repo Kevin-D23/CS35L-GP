666a4f83ef3027a13ee6afeb224a4848bd310c1c
Kevin-D23 <116064184+Kevin-D23@users.noreply.github.com>
2024-01-30 02:14:49 -0800

    Google Auth added

---
+++ src/app/api/auth/[...nextauth]/options.js
@@ -0,0 +1,17 @@
+import GoogleProvider from "next-auth/providers/google";
+
+export const options = {
+  providers: [
+    GoogleProvider({
+      profile(profile) {
+        return {
+          ...profile,
+          id: profile.sub,
+        };
+      },
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET
+};
