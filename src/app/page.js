b2b2846414f85b37219e345ba2895a59203f9934
Kevin-D23 <116064184+Kevin-D23@users.noreply.github.com>
2024-02-01 00:55:23 -0800

    Navbar design changed and buttons added

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,12 @@ import { getServerSession } from "next-auth";
 import {options } from './api/auth/[...nextauth]/options'
 import Link from "next/link";
 
+
 export default async function Home() {
   const session = await getServerSession(options)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {session ? <div><button><Link href="/api/auth/signout?callbackUrl=/">Sign Out</Link></button><h1>{session.user.name} is logged in</h1></div>:
-      <button><Link href="/api/auth/signin">Sign In</Link></button>}
+      
     </main>
   );
 }
