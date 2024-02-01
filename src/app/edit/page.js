328e7a8d7061dbdf477ebde7206cd9d64d9a7a04
Kevin-D23 <116064184+Kevin-D23@users.noreply.github.com>
2024-02-01 14:32:24 -0800

    Code formatting

--- src/app/edit/page.js
+++ src/app/edit/page.js
@@ -1,48 +1,79 @@
 import React from "react";
-import styles from "./stylesUserEdit.module.css";
-import Image from "next/image"
+import styles from "../../../CSS Modules/stylesUserEdit.module.css";
+import Image from "next/image";
 import Link from "next/link";
 import { getServerSession } from "next-auth";
 import { IoMdExit } from "react-icons/io";
 import { options } from "../api/auth/[...nextauth]/options";
 
 function EditButton() {
-    return <button className={`${styles.largerText} ${styles.largerShiftedText}`}>edit</button>;
-  }
+  return (
+    <button className={`${styles.largerText} ${styles.largerShiftedText}`}>
+      edit
+    </button>
+  );
+}
 
+let times = [
+  "Saturday",
+  "Sunday",
+  "Thursdays",
+  "Fridays",
+  "Mondays",
+  "Tuesdays",
+];
+let classes = ["CS35L", "Intro to being swag", "Naptime 101"];
+let hobby = ["eat", "sleep", "nap"];
+let destinations = ["Sproul", "Rieber", "Hendrick"];
+export default async function edit() {
+  const session = await getServerSession(options);
 
-  let times = ['Saturday', 'Sunday', 'Thursdays', 'Fridays','Mondays','Tuesdays'];
-  let classes = ['CS35L', 'Intro to being swag', 'Naptime 101'];
-  let hobby = ['eat', 'sleep', 'nap'];
-  let destinations = ['Sproul', 'Rieber', 'Hendrick'];
-export default async function edit()
-{
-    const session = await getServerSession(options);
-
-    let name = session?.user?.name.split(" ");
-    let email = session?.user?.email;
-      return (
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <h1 className={`${styles.bigHeading} ${styles.largerShiftedText}`}>{name[0]+" "+name[1]}</h1>
-            <EditButton />
-          </div>
-          <div>
-          <h2 className={`${styles.largerText} ${styles.largerShiftedText}`}>{email}</h2>
-          <Image src={session.user.image} width={50} height={50} style={{ marginLeft: '100px' }}/>
-          <Image src={session.user.image} width={50} height={50} style={{ marginLeft: '100px' }}/>
-          
-        <h1 className={`${styles.largerText} ${styles.largerShiftedText}`}>Classes:</h1>
-      <div className={`${styles.scrollableList} ${styles.largerShiftedText}`}>
-        <ul>
-          {classes.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
+  let name = session?.user?.name.split(" ");
+  let email = session?.user?.email;
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <h1 className={`${styles.bigHeading} ${styles.largerShiftedText}`}>
+          {name[0] + " " + name[1]}
+        </h1>
+        <EditButton />
       </div>
-    </div>
-    <h1 className={`${styles.largerText} ${styles.largerShiftedText}${styles.linespaced}`}>space:</h1>
-    <h1 className={`${styles.largerText} ${styles.largerShiftedText}`}>Times Available:</h1>
+      <div>
+        <h2 className={`${styles.largerText} ${styles.largerShiftedText}`}>
+          {email}
+        </h2>
+        <Image
+          src={session.user.image}
+          width={50}
+          height={50}
+          style={{ marginLeft: "100px" }}
+        />
+        <Image
+          src={session.user.image}
+          width={50}
+          height={50}
+          style={{ marginLeft: "100px" }}
+        />
+
+        <h1 className={`${styles.largerText} ${styles.largerShiftedText}`}>
+          Classes:
+        </h1>
+        <div className={`${styles.scrollableList} ${styles.largerShiftedText}`}>
+          <ul>
+            {classes.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <h1
+        className={`${styles.largerText} ${styles.largerShiftedText}${styles.linespaced}`}
+      >
+        space:
+      </h1>
+      <h1 className={`${styles.largerText} ${styles.largerShiftedText}`}>
+        Times Available:
+      </h1>
       <div className={`${styles.scrollableList} ${styles.largerShiftedText}`}>
         <ul>
           {times.map((item, index) => (
@@ -50,8 +81,14 @@ export default async function edit()
           ))}
         </ul>
       </div>
-      <h1 className={`${styles.largerText} ${styles.largerShiftedText}${styles.linespaced}`}>space:</h1>
-      <h1 className={`${styles.largerText} ${styles.largerShiftedText}`}>Prefered Locations:</h1>
+      <h1
+        className={`${styles.largerText} ${styles.largerShiftedText}${styles.linespaced}`}
+      >
+        space:
+      </h1>
+      <h1 className={`${styles.largerText} ${styles.largerShiftedText}`}>
+        Prefered Locations:
+      </h1>
       <div className={`${styles.scrollableList} ${styles.largerShiftedText}`}>
         <ul>
           {destinations.map((item, index) => (
@@ -59,8 +96,14 @@ export default async function edit()
           ))}
         </ul>
       </div>
-      <h1 className={`${styles.largerText} ${styles.largerShiftedText}${styles.linespaced}`}>space:</h1>
-      <h1 className={`${styles.largerText} ${styles.largerShiftedText}`}>Hobbies:</h1>
+      <h1
+        className={`${styles.largerText} ${styles.largerShiftedText}${styles.linespaced}`}
+      >
+        space:
+      </h1>
+      <h1 className={`${styles.largerText} ${styles.largerShiftedText}`}>
+        Hobbies:
+      </h1>
       <div className={`${styles.scrollableList} ${styles.largerShiftedText}`}>
         <ul>
           {hobby.map((item, index) => (
@@ -69,8 +112,5 @@ export default async function edit()
         </ul>
       </div>
     </div>
-    
-    
-    
-      );
-    }
+  );
+}
