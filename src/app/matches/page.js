54cf64cd8a8808759d3fc2037df6c39a2ac2e77f
Kevin-D23 <116064184+Kevin-D23@users.noreply.github.com>
2024-02-09 11:04:41 -0800

    matches page

--- src/app/matches/page.js
+++ src/app/matches/page.js
@@ -1,3 +1,37 @@
+import Link from "next/link";
+import styles from "../../../CSS Modules/matches.module.css";
+
 export default function matches() {
-    return <></>
+  const matches = [
+    { name: "Haohan", userID: "1234556789", email: "haohan@gmail.com" },
+    { name: "Michael", userID: "987654321", email: "michael@yahoo.com" },
+    { name: "Brittany", userID: "294829102", email: "brittany@hotmail.com" },
+    { name: "Amanda", userID: "293401923", email: "amanda@g.ucla.edu.com" },
+  ];
+
+  return (
+    <div className={styles.matchesContainer}>
+      <h1>Matches</h1>
+      <hr />
+      <div className={styles.usersContainer}>
+        {matches.map((user, key) => {
+          return (
+            <div key={key} className={styles.user}>
+              <p className={styles.name}>{user.name}</p>
+              <Link className={styles.profile} href={"/" + user.userID}>
+                View Profile
+              </Link>
+              <a
+                className={styles.email}
+                href={"mailto:" + user.email}
+                type="email"
+              >
+                {user.email}
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
