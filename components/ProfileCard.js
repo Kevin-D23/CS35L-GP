e9fde5beaf4bb03a4a765c5147ccf7994275f6aa
Kevin-D23 <116064184+Kevin-D23@users.noreply.github.com>
2024-02-10 22:17:54 -0800

    Added temp info for when we fetch data

---
+++ components/ProfileCard.js
@@ -0,0 +1,141 @@
+"use client";
+import { useState } from "react";
+import styles from "../CSS Modules/home.module.css";
+
+export default function ProfileCard(userArr) {
+  const [users, setUsers] = useState(userArr.userArr);
+  const [user, setUser] = useState(users[0]);
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  function shiftArr() {
+    setUser(null);
+    setTimeout(() => {
+      if (users.length > 1) {
+        let temp = users;
+        temp.shift();
+        setUsers(temp);
+        setUser(users[0]);
+      }
+    }, 1000);
+  }
+
+  function calcTime(start, end) {
+    let startTime = start + "am";
+    let endTime = end + "am";
+    if (start > 12) startTime = start - 12 + "pm";
+    if (end > 12) endTime = end - 12 + "pm";
+    return startTime + "-" + endTime;
+  }
+
+  return (
+    <>
+      {user ? (
+        <>
+          <div className={styles.matchOption}>
+            <button onClick={() => shiftArr()}>no likey :{"("}</button>
+          </div>
+          <div className={styles.userContainer}>
+            <div className={styles.left}>
+              <div className={styles.imageContainer}></div>
+              <div className={styles.bioContainer}>
+                <h1>{user.name.toUpperCase()}</h1>
+                <h2>
+                  {user.year}
+                  {user.year === 1
+                    ? "st"
+                    : user.year == 2
+                    ? "nd"
+                    : user.year === 3
+                    ? "rd"
+                    : "th"}{" "}
+                  year
+                </h2>
+                <h2>
+                  Major:<p>{user.major}</p>
+                </h2>
+                <p>{user.bio}</p>
+              </div>
+            </div>
+            <div className={styles.right}>
+              <div className={styles.coursesContainer}>
+                <h2>COURSES</h2>
+                <ul>
+                  {user.courses.map((course, key) => {
+                    return <li key={key}>{course}</li>;
+                  })}
+                </ul>
+              </div>
+              <div className={styles.timesContainer}>
+                <h2>AVAILABLE DAYS</h2>
+                <ul>
+                  {user.days.map((day, key) => {
+                    return <li key={key}>{days[day]}</li>;
+                  })}
+                </ul>
+                <h2>WHEN</h2>
+                <p>{calcTime(user.timeStart, user.timeEnd)}</p>
+              </div>
+              <div className={styles.locationContainer}>
+                <h2>LOCATIONS</h2>
+                <ul>
+                  {user.locations.map((location, key) => {
+                    return <li key={key}>{location}</li>;
+                  })}
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className={styles.matchOption}>
+            <button onClick={() => shiftArr()}>me likey :{")"}</button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className={styles.matchOption}>
+            <button onClick={() => shiftArr()}>no likey :{"("}</button>
+          </div>
+          <div className={styles.userContainer}>
+            <div className={styles.left}>
+              <div className={styles.imageContainer}></div>
+              <div className={styles.bioContainer}>
+                <h1>...</h1>
+                <h2>...</h2>
+                <h2>
+                  Major:<p>...</p>
+                </h2>
+                <p>...</p>
+              </div>
+            </div>
+            <div className={styles.right}>
+              <div className={styles.coursesContainer}>
+                <h2>COURSES</h2>
+                ...
+              </div>
+              <div className={styles.timesContainer}>
+                <h2>AVAILABLE DAYS</h2>
+                ...
+                <h2>WHEN</h2>
+                <p>...</p>
+              </div>
+              <div className={styles.locationContainer}>
+                <h2>LOCATIONS</h2>
+                ...
+              </div>
+            </div>
+          </div>
+          <div className={styles.matchOption}>
+            <button onClick={() => shiftArr()}>me likey :{")"}</button>
+          </div>
+        </>
+      )}
+    </>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,97 +2,39 @@ import Image from "next/image";
 import { getServerSession } from "next-auth";
 import { options } from "./api/auth/[...nextauth]/options";
 import styles from "../../CSS Modules/home.module.css";
+import ProfileCard from "../../components/ProfileCard";
 
 export default async function Home() {
   const session = await getServerSession(options);
-  const user = {
-    name: "Haohan Chen",
-    year: 3,
-    major: "Computer Science",
-    bio: "I bully kids blah blah blah blah blah blah blah blah blah",
-    courses: ["CS-33", "Jazz Appreciation", "PHSYC-1A"],
-    days: [0, 1, 2, 3, 4, 5, 6],
-    timeStart: 18,
-    timeEnd: 21,
-    locations: ["your mom's house", "Powell"],
-  };
-  const days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
 
-  function calcTime(start, end) {
-    let startTime = start + "am";
-    let endTime = end + "am";
-    if (start > 12) startTime = start - 12 + "pm";
-    if (end > 12) endTime = end - 12 + "pm";
-    return startTime + "-" + endTime;
-  }
+  const users = [
+    {
+      name: "Haohan Chen",
+      year: 3,
+      major: "Computer Science",
+      bio: "I bully kids blah blah blah blah blah blah blah blah blah",
+      courses: ["CS-33", "Jazz Appreciation", "PHSYC-1A"],
+      days: [0, 1, 2, 3, 4, 5, 6],
+      timeStart: 18,
+      timeEnd: 21,
+      locations: ["your mom's house", "Powell"],
+    },
+    {
+      name: "Kevin Duong",
+      year: 2,
+      major: "Computer Science",
+      bio: "Hi mom",
+      courses: ["CS-33", "CS-M151A"],
+      days: [0, 1, 4, 5, 6],
+      timeStart: 2,
+      timeEnd: 10,
+      locations: ["in my basement"],
+    },
+  ];
 
   return (
     <main className={styles.home}>
-      <div className={styles.matchOption}>
-        <button>no likey :{"("}</button>
-      </div>
-      <div className={styles.userContainer}>
-        <div className={styles.left}>
-          <div className={styles.imageContainer}></div>
-          <div className={styles.bioContainer}>
-            <h1>{user.name.toUpperCase()}</h1>
-            <h2>
-              {user.year}
-              {user.year === 1
-                ? "st"
-                : user.year == 2
-                ? "nd"
-                : user.year === 3
-                ? "rd"
-                : "th"}{" "}
-              year
-            </h2>
-            <h2>
-              Major:<p>{user.major}</p>
-            </h2>
-            <p>{user.bio}</p>
-          </div>
-        </div>
-        <div className={styles.right}>
-          <div className={styles.coursesContainer}>
-            <h2 style={{ marginTop: "20px" }}>COURSES</h2>
-            <ul>
-              {user.courses.map((course, key) => {
-                return <li key={key}>{course}</li>;
-              })}
-            </ul>
-          </div>
-          <div className={styles.timesContainer}>
-            <h2>AVAILABLE DAYS</h2>
-            <ul>
-              {user.days.map((day, key) => {
-                return <li key={key}>{days[day]}</li>;
-              })}
-            </ul>
-            <h2>WHEN</h2>
-            <p>{calcTime(user.timeStart, user.timeEnd)}</p>
-          </div>
-          <div className={styles.locationContainer}>
-            <h2>LOCATIONS</h2>
-            <ul>
-              {user.locations.map((location, key) => {
-                return <li key={key}>{location}</li>;
-              })}
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className={styles.matchOption}>
-        <button>me likey :{")"}</button>
-      </div>
+      <ProfileCard userArr={users} />
     </main>
   );
 }
