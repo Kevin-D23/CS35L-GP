55870e6ebbf1c4833b587935ecd2c418107957bb
Kevin-D23 <116064184+Kevin-D23@users.noreply.github.com>
2024-02-24 20:06:17 -0800

    Changes to groups page

--- src/app/groups/page.js
+++ src/app/groups/page.js
@@ -1,53 +1,44 @@
 "use client";
 import styles from "../../../CSS Modules/groups.module.css";
 import { useRouter } from "next/navigation";
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
 
 export default function Groups() {
-    const[isOpen, setIsOpen] = useState(false);
+  const groups = [
+    { name: "Group 1", members: ["Member1", "Member 2"] },
+    { name: "Group 2", members: ["Member1", "Member 2"] },
+  ];
+
+  function showGroup(group) {
+    const [isOpen, setIsOpen] = useState(false);
 
     const toggleMenu = () => {
-        setIsOpen(!isOpen); // toggle menu visibility
+      setIsOpen(!isOpen); // toggle menu visibility
     };
 
     return (
+      <div>
         <div>
-            <button className={styles.group} onClick={toggleMenu}>
-                CS35L Project Group
-            </button>
-            <div className={`styles.members ${isOpen ? 'open' : ''}`}>
-                <a href="#" className={styles.members}>Member 1</a>
-                <a href="#" className={styles.members}>Member 2</a>
-                <a href="#" className={styles.members}>Member 3</a>
-                <a href="#" className={styles.members}>Member 4</a>
-                <a href="#" className={styles.members}>Member 5</a>
-            </div>
-            
-            <button onClick={toggleMenu} className={styles.group}>
-                Physics 1B Study Group
-            </button>
-            <div className={`styles.members ${isOpen ? 'open' : ''}`}>
-                <a href="#" className={styles.members}>Member 1</a>
-                <a href="#" className={styles.members}>Member 2</a>
-                <a href="#" className={styles.members}>Member 3</a>
-                <a href="#" className={styles.members}>Member 4</a>
-            </div>
-
-            <button onClick={toggleMenu} className={styles.group}>
-                Political Science Research Group
-            </button>
-            <div className={`styles.members ${isOpen ? 'open' : ''}`}>
-                <a href="#" className={styles.members}>Member 1</a>
-                <a href="#" className={styles.members}>Member 2</a>
-                <a href="#" className={styles.members}>Member 3</a>
-            </div>
+          <button className={styles.group} onClick={() => toggleMenu()}>
+            {group.name}
+          </button>
+          <div className={`styles.members ${isOpen ? "styles.open" : ""}`}>
+            {group.members.map((member, i) => {
+              return (
+                <a
+                  key={i}
+                  href="#"
+                  className={`${styles.members} ${isOpen ? styles.open : ""}`}
+                >
+                  Member 1
+                </a>
+              );
+            })}
+          </div>
         </div>
+      </div>
     );
-};
+  }
 
-
-
-
-
-
+  return <div>{groups.map((group) => showGroup(group))}</div>;
+}
