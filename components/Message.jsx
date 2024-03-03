a65cc875ec38f4bdae8ec2fea733120d62b02a84
Tangerine-maker <hchen417@gmail.com>
2024-03-02 17:32:50 -0800

    Expanded on basic layout of a message
    Added placeholder icons for profile pics for specific
    locations. More CSS also for the text and sizes relative
    to the users screen size. Also added an animation for
    profiles.

--- components/Message.jsx
+++ components/Message.jsx
@@ -4,14 +4,17 @@ import styles from "../CSS Modules/messages.module.css"
 function Sender(props){
     return <header className={`${styles.messagePart} ${styles.sender}`}>
       <img src="/icons/scissors.png" className={styles.senderProfile}></img>
-      {props.sender}
+      <div className={styles.senderName}>{props.sender}</div>
       </header>
 }
 function MessageBody(props){
   return <div className={`${styles.messagePart} ${styles.messageBody}`}>{props.contents}</div>
 }
 function Recipients(props){
-  return (<div className={`${styles.messagePart} ${styles.recipients}`}> Me</div>)
+  return (<div className={`${styles.messagePart} ${styles.recipients}`}> 
+    <img src="/icons/scissors.png" className={styles.sendeeProfile}></img>
+    <div className={styles.sendeeName}>Haohan</div>
+  </div>)
 }
 export default function Message(props){ 
     return( <div className={styles.box}>
