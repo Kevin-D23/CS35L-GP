249c2e9785160a894b5a8cd661885e74cc3b62dc
Britney Chen <britneychen872@gmail.com>
2024-03-06 17:49:42 -0800

    added suggested groups to groups page

--- src/app/groups/page.js
+++ src/app/groups/page.js
@@ -9,6 +9,12 @@ export default function Groups() {
     { name: "Physics Lab 3", joinStatus: true, location: "YRL", time: "6:00 pm - 9:00 pm", members: ["Member 1", "Member 2"] },
   ];
 
+  const suggestions = [
+    { name: "Bruins Art Club", joinStatus: false, location: "Covel Commons", time: "7:00 pm - 9:00 pm", members: ["Member 1", "Member 2"] },
+    { name: "Physics 1B Study Group", joinStatus: false, location: "Physics and Astronomy Building", time: "4:00 pm - 6:00 pm", members: ["Member 1", "Member 2"] },
+    { name: "Polisci Research Group", joinStatus: false, location: "Public Affairs Building", time: "9:00 am - 12:00 pm", members: ["Member 1", "Member 2"] },
+  ];
+
   function showGroup(group) {
     const [isOpen, setIsOpen] = useState(false);
     const [joined, setJoined] = useState(true);
@@ -61,10 +67,77 @@ export default function Groups() {
           <button className={styles.exit} onClick={exitGroup}>
             Exit {group.name}
           </button>
+
         </div>
       </div>
     );
   }
 
-  return <div>{groups.map((group) => showGroup(group))}</div>;
+    function showSuggestions(suggestions) {
+      const [isOpen, setIsOpen] = useState(false);
+      const [joined, setJoined] = useState(false);
+      
+      const toggleMenu = () => {
+        setIsOpen(!isOpen); // toggle menu visibility
+      };
+  
+      const joinGroup = () => {
+        console.log(suggestions.name + ' group joined')
+        setJoined(true);
+      }
+  
+      return (
+        <div>
+          <div>
+            <button className={styles.suggestions} onClick={() => toggleMenu()}>
+              {suggestions.name}
+            </button>
+  
+            <div className={`styles.location ${isOpen ? "styles.open" : ""}`}>
+              <a 
+                className={`${styles.location} ${isOpen ? styles.openLocationTime : ""}`}
+              >
+                Meeting location: {suggestions.location}
+              </a>
+            </div>
+  
+            <div className={`styles.time ${isOpen ? "styles.open" : ""}`}>
+              <a 
+                className={`${styles.members} ${isOpen ? styles.openLocationTime : ""}`}
+              >
+                Meeting time: {suggestions.time}
+              </a>
+            </div>
+  
+            <div className={`styles.members ${isOpen ? "styles.open" : ""}`}>
+              {suggestions.members.map((member, i) => {
+                return (
+                  <a
+                    key={i}
+                    href="#"
+                    className={`${styles.members} ${isOpen ? styles.open : ""}`}
+                  >
+                    {member}
+                  </a>
+                );
+              })}
+            </div>
+            <button className={styles.join} onClick={joinGroup}>
+              Join {suggestions.name}
+            </button>
+  
+          </div>
+        </div>
+      );
+  }
+
+  return (
+  <div>
+    <h1 className={styles.title}>YOUR GROUPS</h1>
+    {groups.map((group) => showGroup(group))}
+    <br/>
+    <h1 className={styles.title}>SUGGESTED GROUPS</h1>
+    {suggestions.map((suggestions) => showSuggestions(suggestions))}
+    <br/>
+  </div>);
 }
