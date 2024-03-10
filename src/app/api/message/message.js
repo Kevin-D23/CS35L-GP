01fd88ca1125c56065bd617f9c937efc2041c5dc
michaelkho <155688621+mkhojastegan@users.noreply.github.com>
2024-03-10 13:46:47 -0700

    Removed testing function for messages

--- src/app/api/message/message.js
+++ src/app/api/message/message.js
@@ -7,27 +7,6 @@ const connect = async () => {
     mongoose.connect(process.env.MONGODB_URI);
   };
 
-export async function generateSameMessage(){
-    const sender = "studentsinferno2024@gmail.com";
-    const reciever = "hchen417@gmail.com";
-    const title = "this is a title";
-    const message = "this is a message";
-
-    const data = {
-        sender: sender,
-        reciever: reciever,
-        title: title,
-        message: message
-    };
-
-    await connect();
-
-    Message.create(data);
-    return(
-      <></>
-    )
-}
-
 // DATA has to be in form
 /*
 const data = {
