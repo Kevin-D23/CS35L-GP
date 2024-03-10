4ce620dcefa29989da665188540ad2759c59734b
michaelkho <155688621+mkhojastegan@users.noreply.github.com>
2024-03-10 12:50:08 -0700

    Generate test message

---
+++ src/app/api/message/testMessage.js
@@ -0,0 +1,28 @@
+import { messageSchema } from "/src/app/(models)/Message.js";
+import Message from "@/app/(models)/Message";
+
+const mongoose = require('mongoose');
+const connect = async () => {
+    mongoose.connect(process.env.MONGODB_URI);
+  };
+
+export async function generateSameMessage(){
+    const sender = "studentsinferno2024@gmail.com";
+    const reciever = "hchen417@gmail.com";
+    const title = "this is a title";
+    const message = "this is a message";
+
+    const data = {
+        sender: sender,
+        reciever: reciever,
+        title: title,
+        message: message
+    };
+
+    await connect();
+
+    Message.create(data);
+    return(
+      <></>
+    )
+}
