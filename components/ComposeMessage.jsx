5d33024db7b4b2a225fa8f389a125c40c90107d6
Britney Chen <britneychen872@gmail.com>
2024-03-11 00:17:46 -0700

    made adjustments to messages CSS design, glowing border feature

--- components/ComposeMessage.jsx
+++ components/ComposeMessage.jsx
@@ -3,6 +3,8 @@ import styles from "../CSS Modules/compose.module.css"
 import React from "react"
 import Select from "react-select";
 import Link from "next/link";
+import { IoMdExit } from "react-icons/io";
+
 export default function ComposeMessage(props){
     const [title, setTitle] = React.useState('');
     const [content, setContent] = React.useState('');
@@ -13,7 +15,7 @@ export default function ComposeMessage(props){
         reciever:target,
         title: title,
         message: content}
-        //props.sendData(data)
+        props.sendData(data)
     }
     const customStyles = {
         control: (provided, state) => ({
@@ -46,19 +48,19 @@ export default function ComposeMessage(props){
       };
     return(
         <div className={styles.box}>
-            <Link className={styles.closeButton} href="/messages">x</Link>
-            <div style={{backgroundColor:"#575757",color:"rgb(180, 164, 204)",fontFamily:"Abel"}}>Message Header:</div> 
+            <Link className={styles.closeButton} href="/messages"><IoMdExit /></Link>
+            <div style={{color:"black",fontSize:"1.3rem",fontFamily:"Abel"}}>Message Header:</div> 
            <div  className={styles.title}
       contentEditable="true"
       id="ComposeMessageTitle"
       onInput={handleInput}
     >
     </div>
-          <div style={{backgroundColor:"#575757",color:"rgb(180, 164, 204)",fontFamily:"Abel"}}>Recipient:</div> 
-            <Select  styles={customStyles} options={matches} onChange={(e) => {
+          <div style={{color:"black",fontSize:"1.3rem",fontFamily:"Abel"}}>Recipient:</div> 
+            <Select  className={styles} options={matches} onChange={(e) => {
                 setTarget(e.value)
             }}></Select>
-              <div style={{backgroundColor:"#575757",color:"rgb(180, 164, 204)",fontFamily:"Abel"}}>Message Body:</div> 
+              <div style={{color:"black",fontSize:"1.3rem",fontFamily:"Abel"}}>Message Body:</div> 
             <div className={styles.contents} contentEditable="true"
              id="ComposeMessageContent"
              onInput={handleInputMessage}></div>
