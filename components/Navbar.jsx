caf5322fe54434cb4c941dde93fbfc7e73008d5f
Kevin-D23 <116064184+Kevin-D23@users.noreply.github.com>
2024-03-15 11:24:07 -0700

    Changed navbar name to display name from database instead of name from google

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,11 +11,11 @@ import { getUser } from "@/app/api/user/route";
 export default async function NavBar() {
   const session = await getServerSession(options);
 
-  let name = session?.user?.name.split(" ");
   let email = session?.user?.email;
 
   let user = await getUser(email)
   let image = user?.image
+  let name = user?.name.split(" ");
   return (
     <>
       {session ? (
